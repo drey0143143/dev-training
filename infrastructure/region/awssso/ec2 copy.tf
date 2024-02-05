e5891fd740c98d0bc402edca9aa598e1dac44f48
oodo_ptcinc <oodo@ptc.com>
2024-02-05 11:17:22 -0600

    bulk changes

--- infrastructure/region/awssso/ec2 copy.tf
+++ infrastructure/region/awssso/ec2 copy.tf
@@ -1,11 +1,11 @@
-module "dev_nginx_servers" {
-  source              = "../../..//modules/ec2"
-  environment         = "dev"
-  instance_type       = "t2.micro"
-  instance_count      = "2"
-  ami_id              = "ami-041feb57c611358bd"
-  security_group_name = "dev-nginx"
- }
+#module "dev_nginx_servers" {
+ # source              = "../../..//modules/ec2"
+ # environment         = "dev"
+ # instance_type       = "t2.micro"
+ # instance_count      = "2"
+ # ami_id              = "ami-041feb57c611358bd"
+ # security_group_name = "dev-nginx"
+ #}
 
 #module "prod_nginx_servers" {
  # source = "../../..//modules/ec2"
@@ -16,12 +16,12 @@ module "dev_nginx_servers" {
  # security_group_name = "prod-nginx"
 #}
 
-module "cloudsentrics" {
-  source = "../../..//modules/ec2"
+#module "cloudsentrics" {
+ # source = "../../..//modules/ec2"
 
-  environment         = "sandbox"
-  instance_type       = "t2.micro"
-  instance_count      = "6"
-  ami_id              = "ami-041feb57c611358bd"
-  security_group_name = "cloudsentrics"
-}
+  #environment         = "sandbox"
+  #instance_type       = "t2.micro"
+  #instance_count      = "6"
+  #ami_id              = "ami-041feb57c611358bd"
+  #security_group_name = "cloudsentrics"
+#}
