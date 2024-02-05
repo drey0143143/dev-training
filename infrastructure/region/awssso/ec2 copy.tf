5d73a7d66959c0e72c2e03c0fe857fa6e25e1b64
oodo_ptcinc <oodo@ptc.com>
2024-02-05 10:56:45 -0600

    bulk changes

--- infrastructure/region/awssso/ec2 copy.tf
+++ infrastructure/region/awssso/ec2 copy.tf
@@ -1,3 +1,12 @@
+module "dev_nginx_servers" {
+  source              = "../../..//modules/ec2"
+  environment         = "dev"
+  instance_type       = "t2.micro"
+  instance_count      = "2"
+  ami_id              = "ami-041feb57c611358bd"
+  security_group_name = "dev-nginx"
+ }
+
 module "prod_nginx_servers" {
   source = "../../..//modules/ec2"
   environment         = "prod"
@@ -20,10 +29,5 @@ module "cloudsentrics" {
 }
 
 provider "aws" {
-  region  = "us-east-1"  # Replace with your AWS region
-  # version = "~> 3.0"  # Uncomment and adjust the version if necessary
-
-  # Optionally, you can specify credentials here, but it's
-  # often better to set them via environment variables or shared credentials files.
+  region = "us-east-1"  
 }
-
