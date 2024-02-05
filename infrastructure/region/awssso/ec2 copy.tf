7867de5f5f7f27821c1931fe1ad6638a1b2abcbe
oodo_ptcinc <oodo@ptc.com>
2024-02-05 10:38:11 -0600

    bulk changes

--- infrastructure/region/awssso/ec2 copy.tf
+++ infrastructure/region/awssso/ec2 copy.tf
@@ -1,12 +1,12 @@
-#module "dev_nginx_servers" {
- # source              = "../../..//modules/ec2"
-  #environment         = "dev"
-  #instance_type       = "t2.micro"
-  #instance_count      = "2"
-  #ami_id              = "ami-041feb57c611358bd"
-  #security_group_name = "dev-nginx"
-  #aws_region          = "us-east-1"
-#}
+module "dev_nginx_servers" {
+  source              = "../../..//modules/ec2"
+  environment         = "dev"
+  instance_type       = "t2.micro"
+  instance_count      = "2"
+  ami_id              = "ami-041feb57c611358bd"
+  security_group_name = "dev-nginx"
+  aws_region          = "us-east-1"
+ }
 
 #module "prod_nginx_servers" {
  #environment         = "prod"
